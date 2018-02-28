f487a973b50c8c74d26eb95129c7a518d3515292
AratioD <antti.heroja@gmail.com>
2018-02-28 21:51:44 +0200

    I changed Thymeleaf to React

--- src/main/java/com/mycompany/kingdom/MessagePrinter.java
+++ src/main/java/com/mycompany/kingdom/MessagePrinter.java
@@ -5,10 +5,20 @@
  */
 package com.mycompany.kingdom;
 
-/**
- *
- * @author antti
- */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MessagePrinter {
-    
+
+    final private MessageService service;
+
+    @Autowired
+    public MessagePrinter(MessageService service) {
+        this.service = service;
+    }
+
+    public void printMessage() {
+        System.out.println(this.service.getMessage());
+    }
 }
